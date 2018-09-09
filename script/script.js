f219134fcc534457189bfec806a30da034450505
hesham <h.abdelaal98@gmail.com>
2018-09-09 06:03:19 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+var x = document.getElementById("my-div");
+var y = document.getElementById("my-div2");
+
+x.onclick = function()  {
+    x.classList.toggle("show");
+};
+y.onclick = function()  {
+    y.classList.toggle("show");
+};
